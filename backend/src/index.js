b0d854e30ca3fd23e66e8a4153595be4f290e7a1
Diego <diegolara7820@gmail.com>
2024-12-02 03:17:44 -0500

    image

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,14 +18,16 @@ import bodyParser from "body-parser";
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
