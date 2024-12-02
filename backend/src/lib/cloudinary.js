63adb5cfe2335260a2bb29361a244767816dcc91
Diego <diegolara7820@gmail.com>
2024-12-02 03:42:56 -0500

    Prueba

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -6,12 +6,8 @@ config();
 
 config({ path: '../var.env' });
 
-console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
-console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
-console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET);
-
 cloudinary.config({
-  cloud_name: Diego,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
