c1fabb7766ce3177551f3d585c720541f9380a3a
Diego <diegolara7820@gmail.com>
2024-12-02 03:39:48 -0500

    prueba

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -11,7 +11,7 @@ console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
 console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET);
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: Diego,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
