a817f7bc861157e2f890d95c0d9aff2f1ff59c77
Diego <diegolara7820@gmail.com>
2024-12-02 04:13:58 -0500

    daño

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,9 +17,9 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Bienvenido WAVE!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Seleccione un chat para comenzar a enviar mensajes.
         </p>
       </div>
     </div>
