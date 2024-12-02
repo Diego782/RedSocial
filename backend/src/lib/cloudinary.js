553a07619d60bb98dad04e3ea15bb4c65736f2e3
Diego <diegolara7820@gmail.com>
2024-12-02 03:23:04 -0500

    Prueba

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -4,6 +4,12 @@ import { config } from "dotenv";
 
 config();
 
+config({ path: '../var.env' });
+
+console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
+console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
