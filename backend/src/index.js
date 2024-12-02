e12368a2498a3d28c0d78e7284b09b1ae515d17a
Diego <diegolara7820@gmail.com>
2024-12-02 02:45:16 -0500

    prueba

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,9 +10,10 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+dotenv.config();
 dotenv.config({ path: './var.env' }); 
 
-dotenv.config();
+
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
